4502e799f42d7da8406f8e9b8d5412b8142fb3dd
Bisrat Gizaw <bisrojc60@gmail.com>
2025-05-25 21:26:57 +0300

    updates

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __enter__(self):
+        self.conn = sqlite3.connect("users.db")
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+
+with DatabaseConnection() as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
